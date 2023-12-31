8029e844d9d98cb53163e57254adb7eb3cd5bd71
Tonyb650 <anthonylbrierly@gmail.com>
2023-12-31 14:59:36 -0800

    Update #103

--- code/103.Binary_Tree_Zigzag_Level_Order_Traversal.js
+++ code/103.Binary_Tree_Zigzag_Level_Order_Traversal.js
@@ -1,33 +1,47 @@
-if (!root) {                                // If root is empty, return empty array
-    return [];
-}
-let q = [];                                 // Initialize deque
-q.push(root);                               // enqueue 'root'
-q.push(null);                               // enqueue null (delimiter)
-let leftToRight = true;                     // set initial direction for adding to subArray
-let subArray = [];                          // Initialize subArray
-let result = [];                            // Initialize result array that will hold final result
-while(q.length > 0){
-    const currentNode = q.shift();          // Dequeue the left-most node as 'currentNode'
-    if(currentNode != null){
-        if(leftToRight){
-            subArray.push(currentNode.val); // if 'left first', add node value to back of array
-        } else { //(RightToLeft)
-            subArray.unshift(currentNode.val); // if 'right first, add node to left front of array
-        }
-        if(currentNode.left){
-            q.push(currentNode.left);       // add left child to queue
-        }
-        if(currentNode.right){
-            q.push(currentNode.right);      // add right child to queue 
-        }
-    } else {
-        result.push(subArray);              // push subArray (level array) to result
-        subArray = [];                      // clear sub
-        leftToRight = !leftToRight;         // reverse leftToRight
-        if(q.length  > 0) {                 // if there is another level to process, add null element (delimiter) to end of queue
-            q.push(null); 
-        } 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    if (!root) {                                // If root is empty, return empty array
+        return [];
     }
-}
-return result;
+    let q = [];                                 // Initialize deque
+    q.push(root);                               // enqueue 'root'
+    q.push(null);                               // enqueue null (delimiter)
+    let leftToRight = true;                     // set initial direction for adding to subArray
+    let subArray = [];                          // Initialize subArray
+    let result = [];                            // Initialize result array that will hold final result
+    while(q.length > 0){
+        const currentNode = q.shift();          // Dequeue the left-most node as 'currentNode'
+        if(currentNode != null){
+            if(leftToRight){
+                subArray.push(currentNode.val); // if 'left first', add node value to back of array
+            } else { //(RightToLeft)
+                subArray.unshift(currentNode.val); // if 'right first, add node to left front of array
+            }
+            if(currentNode.left){
+                q.push(currentNode.left);       // add left child to queue
+            }
+            if(currentNode.right){
+                q.push(currentNode.right);      // add right child to queue 
+            }
+        } else {
+            result.push(subArray);              // push subArray (level array) to result
+            subArray = [];                      // clear sub
+            leftToRight = !leftToRight;         // reverse leftToRight
+            if(q.length  > 0) {                 // if there is another level to process, 
+                q.push(null);                   // add null element (delimiter) to end of queue
+            } 
+        }
+    }
+    return result;
+};
