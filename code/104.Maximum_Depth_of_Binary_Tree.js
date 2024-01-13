a08a22e19a343a63bc31615da68dfcc15177ea9f
Tonyb650 <anthonylbrierly@gmail.com>
2024-01-13 08:39:07 -0800

    Add #104 bottom up solution

--- code/104.Maximum_Depth_of_Binary_Tree.js
+++ code/104.Maximum_Depth_of_Binary_Tree.js
@@ -28,3 +28,25 @@ var maxDepth = function(root) {
   testNode(root,1);                               // start us off with root node at level 1
   return result;
 };
+
+// BOTTOM UP SOLUTION 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if(root == null){                               // Recursive base case:
+      return 0;                                   // At any point, when we get to null, return '0'
+  }
+  let leftDepth = maxDepth(root.left) + 1;        // For every call on the stack, add one (left)
+  let rightDepth = maxDepth(root.right) + 1;      // Same for the right side of the given node
+  return Math.max(leftDepth, rightDepth);         // At every single node, we compare the result from both sides, and return whatever is larger at that point
+};
