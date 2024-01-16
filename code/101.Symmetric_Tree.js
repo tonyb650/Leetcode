43dfb219e91651d161a7d6eb0b27149311b955f3
Tonyb650 <anthonylbrierly@gmail.com>
2024-01-16 08:16:37 -0800

    Add 101 Symmetric Tree iterative solution

--- code/101.Symmetric_Tree.js
+++ code/101.Symmetric_Tree.js
@@ -1,39 +1,17 @@
-### 101. Symmetric Tree
+// * RECURSIVE SOLUTION * 
 
-**Problem**\
-Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
-
-**Example 1:**
-```
-      1
-    /   \
-   2     2
-  / \    / \
- 3   4  4   3
-```
-Input: root = [1,2,2,3,4,4,3]\
-Output: true
-
-**Example 2:**
-```
-      1
-    /   \
-   2     2
-    \     \
-     3     3
-```
-Input: root = [1,2,2,null,3,null,3]\
-Output: false
-
-**Constraints:**
-- The number of nodes in the tree is in the range [1, 1000].
-- -100 <= Node.val <= 100
- 
-**Follow up:** Could you solve it both recursively and iteratively?
-
----
-### Solution, Javascript: recursive
-```javascript
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 var isSymmetric = function(root) {
   if(!root) {
     return true;
@@ -52,11 +30,21 @@ var isSymmetric = function(root) {
   }
   return helper(root.left,root.right)
 };
-```
----
 
-### Solution, Javascript: iterative
-```javascript
+// * ITERATIVE SOLUTION *
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
 var isSymmetric = function(root) {
   if(!root || (root.left == null && root.right == null)) {
       return true;
@@ -81,5 +69,3 @@ var isSymmetric = function(root) {
   }
   return true
 };
-```
----
